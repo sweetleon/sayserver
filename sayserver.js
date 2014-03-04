5bd02e06ad34f6e9ccda38979b2fd33c3741eda6
Lenny Turetsky and Ryan Tang <pair+lenny+ryantang@pivotallabs.com>
2014-03-04 12:01:23 -0800

    initial version

---
+++ sayserver.js
@@ -0,0 +1,40 @@
+var http = require("http");
+var url  = require('url');
+
+var sys = require('sys');
+var exec = require('child_process').exec;
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+
+    var url_parts = url.parse(request.url, true);
+    var query = url_parts.query;
+
+    console.log(query.message);
+
+    if (!query.message) return;
+
+    response.write("Saying: ");
+    response.write("" + query.message);
+    response.write("second time shows" + query.message);
+
+//    console.log(query.message);
+
+    var command = "say '" + query.message + "'";
+    console.log(command);
+    child = exec(command, function (error, stdout, stderr) {
+        if (error !== null) {
+            console.log('exec error: ' + error);
+            console.log("STDOUT: " + stdout);
+            console.log("STDERR: " + stderr);
+            response.write("ERROR: " + error);
+            response.write("STDOUT: " + stdout);
+            response.write("STDERR: " + stderr);
+        }
+    });
+//    child.stdin.
+
+    response.end("");
+
+    return query.message;
+}).listen(8888);
